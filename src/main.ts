a2bf6511dc5644d2a47b758b79405e0869472270
mechztcc <dojathan@gmail.com>
2022-06-20 14:14:31 -0400

    feat: add dotenv pattern

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
+import 'dotenv/config';
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ forbidNonWhitelisted: true, whitelist: true }))
+  app.useGlobalPipes(
+    new ValidationPipe({ forbidNonWhitelisted: true, whitelist: true }),
+  );
   await app.listen(3000);
 }
 bootstrap();
