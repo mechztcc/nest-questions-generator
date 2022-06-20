723bd954eb700abab3ea3e899929d76c004c80d3
mechztcc <dojathan@gmail.com>
2022-06-20 14:42:57 -0400

    feat: configure auth module

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,7 +1,24 @@
 import { Module } from '@nestjs/common';
-import { Auth } from './services/auth';
+import { JwtModule } from '@nestjs/jwt';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PassportModule } from '@nestjs/passport';
+import { UsersSchema } from '../users/schemas/user.schema';
+import { AuthService } from './services/auth.service';
 
 @Module({
-  providers: [Auth]
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: 'User',
+        schema: UsersSchema,
+      },
+    ]),
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: process.env.JWT_EXPIRATION },
+    }),
+  ],
+  providers: [AuthService],
 })
 export class AuthModule {}
