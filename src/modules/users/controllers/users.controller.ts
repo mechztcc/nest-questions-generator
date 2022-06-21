a9cc59194192d1d133b8be4b5528347ec6a29a1a
mechztcc <dojathan@gmail.com>
2022-06-21 09:11:04 -0400

    feat: add signup controller

--- src/modules/users/controllers/users.controller.ts
+++ src/modules/users/controllers/users.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Inject, Post } from '@nestjs/common';
+import { SignupDto } from '../dtos/signup.dto';
+import { User } from '../models/users.model';
+import { UsersService } from '../services/users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post('signup')
+  public async signup(@Body() signupDto: SignupDto): Promise<User> {
+    return this.usersService.signup(signupDto);
+  }
+}
